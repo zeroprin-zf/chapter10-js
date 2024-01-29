76d4cb32a4e086f43a8a80a9795bc1f3488e6d00
zeroprin-zf <82091884+zeroprin-zf@users.noreply.github.com>
2024-01-29 12:36:56 +0000

    完成

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ $(function(){
   $('.menu-trigger').on('click', function(event){
     //.menu-triggerクリック時に行われる処理
     $(this).toggleClass('active');//トグル処理、該当のclass属性がある場合にはclassが削除され、ない場合は追加される。
-    $('#sp-menu').fadeToggle();
+    //$(this)アイコンが切り替わる。クリックされた要素自身を表す。
+    $('#sp-menu').fadeToggle();//要素のフェードイン・フェードアウトを切り替えるメソッド
+    //このメソッドを使って開いた時の状態を設定する
+    //sp-menuが開いたときに処理されるように設定する
+    //menuのところに設定している
     event.preventDefault();
   });
 });
